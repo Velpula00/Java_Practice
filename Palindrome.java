4cb52f62ace74b647dfa320486190df8a8a80a72
Pavan Velpula <106097739+Velpula00@users.noreply.github.com>
2022-07-26 10:03:26 +0530

    Add files via upload

--- Palindrome.java
+++ Palindrome.java
@@ -1,24 +1,42 @@
-//Write a JAVA program to check whether a number is palindrome or not.
-import java.util.*;
-class Palindrome
+/*  JFM1T8_Assignment4:
+
+    Write a program to check whether a string is a Palindrome.
+    Prompt the user input from the terminal.
+    
+    Sample input: Malayalam
+    
+    Expected output: Malayalam is a Palindrome
+*/
+
+import java.util.Scanner;
+
+public class Palindrome {
+
+//main method
+  public static void main(String args[])
   {
-    public static void main(String[]args)
+//declare varible
+    String rev="";
+//take input from user
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter a string");
+    String str=sc.nextLine();
+/* 
+   divide the whole string into two halves and compare each character in the 
+   first half with each character in the second half in reverse.  
+   If both are equal then it is a palindrome else print it is not a palindrome 
+*/
+    for(int i=str.length()-1;i>=0;i--)
+      {
+        rev=rev+str.charAt(i);
+      }
+    
+    if(str.toLowerCase().equals(rev.toLowerCase()))
     {
-      Scanner sc=new Scanner(System.in);
-      int no,b,rev=0,c;
-      System.out.println("Enter any number:");
-      no=sc.nextInt();
-      c=no;
-      while(no!=0)
-        {
-          b=no%10;
-          rev=rev*10+b;
-          no=no/10;
-          
-        }
-      if(c==rev)
-        System.out.println("Num is palindrome");
-      else
-        System.out.println("Num is not a palindrome");
+      System.out.println(str+" is a polindrome");
+    }
+    else{
+      System.out.println(str+" is not a polindrome");
     }
   }
+}
